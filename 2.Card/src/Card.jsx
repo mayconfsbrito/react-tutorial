d4745202a783492e042f3dd52f112b76fcd9c979
Maycon Brito <maycon@clubefii.com.br>
2024-07-09 20:26:40 -0300

    Primeiro commit

---
+++ 2.Card/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card(){
+    return(
+        <div className="card">
+            <img></img>
+            <h2>Bro Code</h2>
+            <p>I make Youtube videos and play video games</p>
+        </div>
+    );
+}
+
+export default Card
